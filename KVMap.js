bb1a1745c9d8a8ac001f33c03714181d4d15992d
geeksnail <geeksnail@outlook.com>
2024-06-08 05:06:53 +0800

    init worker

---
+++ KVMap.js
@@ -0,0 +1,168 @@
+export { default as inCfSubNet } from "./cfcidr";
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+const union = (a, b) => [ ...new Set([...a, ...b]) ];
+const difference = (a, b) => {
+	const s = new Set(a);
+	for (const e of b) 
+		s.delete(e);
+	return [...s];
+};
+function symmetricDifference(a, b, seperate=false) {
+	const _a = new Set(a);
+	let _b;
+	if (seperate) _b = new Set();
+	else _b = _a;
+	for (const e of b) 
+		_a.has(e)? _a.delete(e): _b.add(e);
+	return seperate? [[..._a], [..._b]]: [..._a];
+}
+const randFrom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const delay = (t) => new Promise(resolve => { setTimeout(resolve, t); });
+
+export default class KVMap {
+	KV;
+	KEY_CFHOST = 'cfhost';
+	KEY_PROXYS = 'proxys';
+	//initial entry
+	_proxys; _cfhost;
+	//cache
+	cachedProxys = []; cachedCfhost = []; 
+	//source
+	cfhostFromKV = [];
+	loadedProxys = false;
+	loadedCfhost = false;
+	puttingCfhost = false;
+	constructor({KV, proxys = [], cfhost = []}) {
+		this.KV = KV;
+		this._proxys = proxys;
+		this._cfhost = cfhost;
+	}
+	get proxys() {
+		return [...this._proxys, ...this.cachedProxys]
+	}
+	get cfhost() {
+		return [...this._cfhost, ...this.cachedCfhost]
+	}
+	get randomProxy() {
+		return randFrom(this.proxys);
+	}
+	loadCfhost() {
+		if (this.loadedCfhost) return;
+		this.KVOp(this.KEY_CFHOST, 'get').then(async r => {
+			if (r) {
+				if (r.find(e => this._cfhost.includes(e))) {
+					r = difference(r, this._cfhost);
+					//update to KV
+					this.puttingCfhost = true;
+					this.KVOp(KEY_CFHOST, 'put', r).then(()=>{ this.puttingCfhost = false; })
+				}
+				this.cfhostFromKV = r;
+				this.cachedCfhost = r;
+			}
+			this.loadedCfhost = true;
+			console.log(`KV ${this.KEY_CFHOST} loaded ${this.cfhost.length}`)
+			return this.cfhost;
+		})
+	}
+	loadProxys() {
+		if (this.loadedProxys) 
+			return Promise.resolve(this.proxys);
+		return this.KVOp(this.KEY_PROXYS, 'get').then(r => {
+			if (r) {
+				this.cachedProxys = r;
+			}
+			this.loadedProxys = true;
+			console.log(`KV ${this.KEY_PROXYS} loaded ${this.proxys.length}`)
+			return this.proxys;
+		})
+	}
+	async deleteProxy(proxy) {
+		while (! this.loadedProxys) {
+			console.log(`${this.KEY_PROXYS} not loaded! try wait 20ms`)
+			await delay(20);
+		}
+		let i = this.cachedProxys.indexOf(proxy);
+		if (i < 0) return;
+		this.cachedProxys.splice(i, 1);
+		this.KVOp(this.KEY_PROXYS, 'put', this.cachedProxys)
+			.then(r => { console.log(`proxy ${proxy} deleted from KV`); })
+	}
+	async tagHost(host) {
+		while (! this.loadedCfhost) {
+			console.log(`${this.KEY_CFHOST} not loaded! try wait 20ms`)
+			await delay(20);
+		}
+		if (! this.cfhost.includes(host))
+			this.cachedCfhost.push(host);
+		else 
+			return;
+		console.log(`cached ${host} ${this.cfhost.length}`)
+		while (this.puttingCfhost) {
+			console.log(`${this.KEY_CFHOST} is been putting! try wait 50ms`)
+			await delay(50);
+		}
+		if (! this.puttingCfhost) {
+			this.puttingCfhost = true;
+			if (this.cfhostFromKV.length) {
+				let r = await this.KVOp(this.KEY_CFHOST, 'get');
+				let [ldiff, rdiff] = symmetricDifference(this.cachedCfhost, r, true);
+				this.cachedCfhost.push(...rdiff);
+				if (ldiff.length)
+					await this.KVOp(this.KEY_CFHOST, 'put', this.cachedCfhost)
+						.then(r => { console.log(`tagged ${ldiff} to KV`); })
+				if (rdiff.length)
+					console.log(`received new cfhost: ${rdiff}, ${r.length}(KV) ${this.cfhost.length}(cache)`)
+			} else {
+				await this.KVOp(this.KEY_CFHOST, 'put', this.cachedCfhost)
+					.then(r => { console.log(`tagged ${this.cachedCfhost} to KV`); })
+			}
+			this.puttingCfhost = false;
+		}
+	}
+	// key, get 
+	// key, put,  string|Array|Object
+	// key, push, arg0, arg1, ...
+	// key, set,  arg0, arg1
+	async KVOp(key, op, ...args){
+		let val;
+		if (op == 'put') {
+			args = args[0]
+		} else if (/push|set/.test(op)) {
+			val = await this.KVApi('get', key);
+			if (val) {
+				if (val instanceof Array) {
+					if (op == 'push') 
+						val.push(...args);
+				} else if (val.toString() == '[object Object]') {
+					if (op == 'set')
+						val[args[0]] = args[1]
+				}
+				args = val
+			}
+			op = 'put'
+		}
+		return await this.KVApi(op, key, args);
+	}
+	/**
+	  * op: get,put,list,delete
+	  * args: key, value(string,object,array)
+	  */
+	async KVApi(op, ...args){
+		let val;
+		if (op == 'get') 
+			args = args.slice(0, 1)
+		try {
+			if (op == 'put' && typeof args[1] == 'object') {
+				args[1] = JSON.stringify(args[1]);
+			}
+			val = await this.KV[op](...args);
+			if (op == 'get' && val && /^\[.*\]$/.test(val)) {
+				val = JSON.parse(val);
+			}
+		} catch (err) {
+			console.error('KV error', err);
+		}
+		return val;
+	}
+}
---
+++ build.js
@@ -0,0 +1,122 @@
+//https://github.com/cloudflare/workers-sdk/blob/main/packages/wrangler/src/deployment-bundle/bundle.ts
+const path = require("path");
+const { NodeGlobalsPolyfillPlugin } = require("@esbuild-plugins/node-globals-polyfill");
+const { NodeModulesPolyfillPlugin } = require("@esbuild-plugins/node-modules-polyfill");
+const esbuild = require('esbuild');
+
+var nodejsCompatPlugin = /* @__PURE__ */ (silenceWarnings) => ({
+  name: "nodejs_compat imports plugin",
+  setup(pluginBuild) {
+    const seen = /* @__PURE__ */ new Set();
+    const warnedPackaged = /* @__PURE__ */ new Map();
+    pluginBuild.onStart(() => {
+      seen.clear();
+      warnedPackaged.clear();
+    });
+    pluginBuild.onResolve(
+      { filter: /node:.*/ },
+      async ({ path: path67, kind, resolveDir, ...opts }) => {
+        const specifier = `${path67}:${kind}:${resolveDir}:${opts.importer}`;
+        if (seen.has(specifier)) {
+          return;
+        }
+        seen.add(specifier);
+        const result = await pluginBuild.resolve(path67, {
+          kind,
+          resolveDir,
+          importer: opts.importer
+        });
+        if (result.errors.length > 0) {
+          let pathWarnedPackaged = warnedPackaged.get(path67);
+          if (pathWarnedPackaged === void 0) {
+            warnedPackaged.set(path67, pathWarnedPackaged = []);
+          }
+          pathWarnedPackaged.push(opts.importer);
+          return { external: true };
+        }
+        return result;
+      }
+    );
+    pluginBuild.onEnd(() => {
+      if (!silenceWarnings) {
+        warnedPackaged.forEach((importers, path67) => {
+          console.warn(
+            `The package "${path67}" wasn't found on the file system but is built into node.
+Your Worker may throw errors at runtime unless you enable the "nodejs_compat" compatibility flag. Refer to https://developers.cloudflare.com/workers/runtime-apis/nodejs/ for more details. Imported from:
+${importers.map(
+              (i) => ` - ${path.relative(pluginBuild.initialOptions.absWorkingDir ?? "/", i)}`
+            ).join("\n")}`
+          );
+        });
+      }
+    });
+  }
+});
+
+var cloudflareInternalPlugin = {
+  name: "Cloudflare internal imports plugin",
+  setup(pluginBuild) {
+    pluginBuild.onResolve({ filter: /^cloudflare:.*/ }, () => {
+      return { external: true };
+    });
+  }
+};
+
+var args = process.argv.slice(2);
+const opt = {
+	entry: '_worker.js',
+	workingDir: '.',
+	outdir: 'dist',
+	nodeCompat: false,
+}
+for (let i=0; i<args.length; i++) {
+	switch (args[i]) {
+		case "--entry": opt.entry = args[++i]; break;
+		case "--out-dir": opt.entry = args[++i]; break;
+		case "--working-dir": opt.workingDir = args[++i]; break;
+		case "--node-compat": opt.nodeCompat = true; break;
+	}
+}
+
+const buildOptions = {
+  bundle: true,
+  entryPoints: [ path.resolve(__dirname, opt.entry) ],
+	absWorkingDir: path.resolve(__dirname, opt.workingDir),
+  outdir: opt.outdir,
+  external: [ '__STATIC_CONTENT_MANIFEST' ],
+  format: 'esm',
+  target: 'es2022',
+  sourcemap: true,
+  sourceRoot: opt.outdir,
+  minify: undefined,
+  metafile: true,
+  conditions: [ 'workerd', 'worker', 'browser' ],
+  define: {
+    'navigator.userAgent': '"Cloudflare-Workers"',
+    'process.env.NODE_ENV': '"undefined"',
+    global: 'globalThis'
+  },
+  loader: { '.js': 'jsx', '.mjs': 'jsx', '.cjs': 'jsx' },
+  plugins: [
+		...(opt.nodeCompat
+			? [ NodeGlobalsPolyfillPlugin({ buffer: true }),
+					NodeModulesPolyfillPlugin(),
+					nodejsCompatPlugin(false)
+				]
+			: []),
+    cloudflareInternalPlugin,
+  ],
+  jsxFactory: 'React.createElement',
+  jsxFragment: 'React.Fragment',
+  logLevel: 'silent'
+};
+
+async function build() {
+	var result;
+	try {
+		result = await esbuild.build(buildOptions);
+	} catch (err) {
+		console.error(err);
+	}
+}
+build();
