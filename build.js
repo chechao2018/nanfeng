8adc5e758dec297c43e6e19aa3060449e6c5fcdc
geeksnail <geeksnail@outlook.com>
2024-07-09 06:14:07 +0800

    verify the cfhost data

--- build.js
+++ build.js
@@ -2,9 +2,9 @@
 const path = require("path");
 const { NodeGlobalsPolyfillPlugin } = require("@esbuild-plugins/node-globals-polyfill");
 const { NodeModulesPolyfillPlugin } = require("@esbuild-plugins/node-modules-polyfill");
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 
-var nodejsCompatPlugin = /* @__PURE__ */ (silenceWarnings) => ({
+var nodejsCompatPlugin = /* @__PURE__ */ silenceWarnings => ({
   name: "nodejs_compat imports plugin",
   setup(pluginBuild) {
     const seen = /* @__PURE__ */ new Set();
@@ -24,12 +24,12 @@ var nodejsCompatPlugin = /* @__PURE__ */ (silenceWarnings) => ({
         const result = await pluginBuild.resolve(path67, {
           kind,
           resolveDir,
-          importer: opts.importer
+          importer: opts.importer,
         });
         if (result.errors.length > 0) {
           let pathWarnedPackaged = warnedPackaged.get(path67);
           if (pathWarnedPackaged === void 0) {
-            warnedPackaged.set(path67, pathWarnedPackaged = []);
+            warnedPackaged.set(path67, (pathWarnedPackaged = []));
           }
           pathWarnedPackaged.push(opts.importer);
           return { external: true };
@@ -43,14 +43,14 @@ var nodejsCompatPlugin = /* @__PURE__ */ (silenceWarnings) => ({
           console.warn(
             `The package "${path67}" wasn't found on the file system but is built into node.
 Your Worker may throw errors at runtime unless you enable the "nodejs_compat" compatibility flag. Refer to https://developers.cloudflare.com/workers/runtime-apis/nodejs/ for more details. Imported from:
-${importers.map(
-              (i) => ` - ${path.relative(pluginBuild.initialOptions.absWorkingDir ?? "/", i)}`
-            ).join("\n")}`
+${importers
+  .map(i => ` - ${path.relative(pluginBuild.initialOptions.absWorkingDir ?? "/", i)}`)
+  .join("\n")}`
           );
         });
       }
     });
-  }
+  },
 });
 
 var cloudflareInternalPlugin = {
@@ -59,56 +59,65 @@ var cloudflareInternalPlugin = {
     pluginBuild.onResolve({ filter: /^cloudflare:.*/ }, () => {
       return { external: true };
     });
-  }
+  },
 };
 
 var args = process.argv.slice(2);
 const opt = {
-	entry: 'src/_worker.js',
-	workingDir: 'src',
-	outdir: 'dist',
-	nodeCompat: false,
-}
-for (let i=0; i<args.length; i++) {
-	switch (args[i]) {
-		case "--entry": opt.entry = args[++i]; break;
-		case "--out-dir": opt.entry = args[++i]; break;
-		case "--working-dir": opt.workingDir = args[++i]; break;
-		case "--node-compat": opt.nodeCompat = true; break;
-	}
+  entry: "src/_worker.js",
+  workingDir: ".",
+  outdir: "dist",
+  nodeCompat: false,
+};
+for (let i = 0; i < args.length; i++) {
+  switch (args[i]) {
+    case "--entry":
+      opt.entry = args[++i];
+      break;
+    case "--out-dir":
+      opt.entry = args[++i];
+      break;
+    case "--working-dir":
+      opt.workingDir = args[++i];
+      break;
+    case "--node-compat":
+      opt.nodeCompat = true;
+      break;
+  }
 }
 
 const buildOptions = {
   bundle: true,
-  entryPoints: [ path.resolve(__dirname, opt.entry) ],
-	absWorkingDir: path.resolve(__dirname, opt.workingDir),
+  entryPoints: [path.resolve(__dirname, opt.entry)],
+  absWorkingDir: path.resolve(__dirname, opt.workingDir),
   outdir: opt.outdir,
-  external: [ '__STATIC_CONTENT_MANIFEST' ],
-  format: 'esm',
-  target: 'es2022',
+  external: ["__STATIC_CONTENT_MANIFEST"],
+  format: "esm",
+  target: "es2022",
   sourcemap: true,
   sourceRoot: opt.outdir,
   minify: undefined,
   metafile: true,
-  conditions: [ 'workerd', 'worker', 'browser' ],
+  conditions: ["workerd", "worker", "browser"],
   define: {
-    'navigator.userAgent': '"Cloudflare-Workers"',
-    'process.env.NODE_ENV': '"undefined"',
-    global: 'globalThis'
+    "navigator.userAgent": '"Cloudflare-Workers"',
+    "process.env.NODE_ENV": '"undefined"',
+    global: "globalThis",
   },
-  loader: { '.js': 'jsx', '.mjs': 'jsx', '.cjs': 'jsx' },
+  loader: { ".js": "jsx", ".mjs": "jsx", ".cjs": "jsx" },
   plugins: [
-		...(opt.nodeCompat
-			? [ NodeGlobalsPolyfillPlugin({ buffer: true }),
-					NodeModulesPolyfillPlugin(),
-					nodejsCompatPlugin(false)
-				]
-			: []),
+    ...(opt.nodeCompat
+      ? [
+          NodeGlobalsPolyfillPlugin({ buffer: true }),
+          NodeModulesPolyfillPlugin(),
+          nodejsCompatPlugin(false),
+        ]
+      : []),
     cloudflareInternalPlugin,
   ],
-  jsxFactory: 'React.createElement',
-  jsxFragment: 'React.Fragment',
-  logLevel: 'silent'
+  jsxFactory: "React.createElement",
+  jsxFragment: "React.Fragment",
+  logLevel: "silent",
 };
 
-esbuild.build(buildOptions).then(console.log)
+esbuild.build(buildOptions).then(console.log);
